0f387493f724d4cd896415fd2728aeab38a0319f
Israel Palma <isra.palma@gmail.com>
2022-06-26 09:50:36 -0500

    Add project files.

---
+++ PhotosApp-LearnWinUI3/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Search;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace PhotosApp_LearnWinUI3
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public ObservableCollection<ImageFileInfo> Images { get; } = new ObservableCollection<ImageFileInfo>();
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            _ = GetItemsAsync();
+        }
+
+        private async Task GetItemsAsync()
+        {
+            StorageFolder appInstalledFolder = Package.Current.InstalledLocation;
+            StorageFolder picturesFolder = await appInstalledFolder.GetFolderAsync("Assets\\Samples");
+
+            var result = picturesFolder.CreateFileQueryWithOptions(new QueryOptions());
+
+            IReadOnlyList<StorageFile> imageFiles = await result.GetFilesAsync();
+
+            foreach(StorageFile file in imageFiles)
+            {
+                Images.Add(await LoadImageInfo(file));
+            }
+
+            ImageGridView.ItemsSource = Images;
+        }
+
+        public async static Task<ImageFileInfo> LoadImageInfo(StorageFile file)
+        {
+            var properties = await file.Properties.GetImagePropertiesAsync();
+            ImageFileInfo info = new(properties, file, file.DisplayName, file.DisplayType);
+            return info;
+
+        }
+
+        
+    }
+
+    // C# Code-behind
+
+    public class MyDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate Normal { get; set; }
+        public DataTemplate Accent { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item)
+        {
+            if ((int)item % 2 == 0)
+            {
+                return Normal;
+            }
+            else
+            {
+                return Accent;
+            }
+        }
+    }
+}
